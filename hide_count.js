884d8d62a9a028ff1f77c0f24abaccc350a2784f
Jonathan Woodard <woodardj@gmail.com>
2012-08-17 10:51:17 -0600

    remove commented-out stuff, hide console.logs

--- hide_count.js
+++ hide_count.js
@@ -1,18 +1,12 @@
-console.log("watching title for parens")
+console.log("Notify-hider loaded. Watching <title>");
 var pattern = /\([0-9]+\)\s/;
 
 // set up an observer for the title element // Magic!
 var target = document.querySelector('head > title');
 var observer = new window.WebKitMutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    console.log('new title:', mutation.target.textContent);
+    //console.log('new title:', mutation.target.textContent);
     remove_parens();
-//    var str = mutation.target.textContent;
-//    if(pattern.test(str)){
-//      new_title = str.replace(pattern,"")
-//      console.log('NEW new title:', new_title);
-//      document.title = new_title;
-//    }
   });
 });
 observer.observe(target, { subtree: true, characterData: true, childList: true });
@@ -25,7 +19,7 @@ function remove_parens(){
   var str = document.title;
   if(pattern.test(str)){
     new_title = str.replace(pattern,"")
-    console.log('NEW new title:', new_title);
+    //console.log('NEW new title:', new_title);
     document.title = new_title;
   }
 }
