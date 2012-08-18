855aff2d39721be503c90e99bd02899ac54dee38
Jonathan Woodard <woodardj@gmail.com>
2012-08-18 17:37:17 -0600

    remove some console.logging

--- background.js
+++ background.js
@@ -3,11 +3,8 @@ var settings = new Store("settings", {
   "twitter": true,
   "gmail": true,
 });
-console.log("gonna register a listener");
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  console.log("someone asked for something");
-  console.log(settings.toObject());
-  console.log(request.action);
   if (request.action == 'gpmeGetOptions') {
     sendResponse(settings.toObject());
   }
